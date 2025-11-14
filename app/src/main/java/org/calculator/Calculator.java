b4bb77c1c92ec2c5614a7bdfcfe0f4668097866c
Colm Callan <x00195992@mytudublin.ie>
2025-11-14 14:50:44 +0000

    prep for vido + suppression

--- app/src/main/java/org/calculator/Calculator.java
+++ app/src/main/java/org/calculator/Calculator.java
@@ -8,13 +8,17 @@ public class Calculator {
         if (b == 0) throw new ArithmeticException("Cannot divide by zero");
         return (double) a / b;
     }
-
+    //     public int square(int a) {
+    //     return a * a;
+    // }
+    @SuppressWarnings("java:S106") // Sonar wants me to use logger but im not sure how to so i am suppressing it
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        int a = 9, b = 3;
+        int a = 9, b = 3; //declare b below
         System.out.println("add: " + calc.add(a, b));
         System.out.println("subtract: " + calc.subtract(a, b));
         System.out.println("multiply: " + calc.multiply(a, b));
         System.out.println("divide: " + calc.divide(a, b));
+        // System.out.println("square: " + calc.square(a));
     }
 }
